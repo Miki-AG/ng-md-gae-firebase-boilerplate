976055d242a395c025b13017b598eb498f6901ae
MIGUEL ARMENGOL-GARCIA <miguel.a.garcia@ocecat.com>
2018-06-02 11:09:32 -0400

    Formatting

--- server/api_heroes.py
+++ server/api_heroes.py
@@ -1,4 +1,4 @@
-# Api for Heroes model
+"""Api for Heroes model."""
 import endpoints
 from google.appengine.ext import ndb
 from protorpc import remote
@@ -6,39 +6,48 @@ from endpoints_proto_datastore.ndb import EndpointsModel
 from endpoints_proto_datastore.ndb import EndpointsAliasProperty
 import logging
 
-
 class Hero(EndpointsModel):
+    """Hero model."""
     _message_fields_schema = ('id', 'name', 'created')
 
     name = ndb.StringProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
-
 
 @endpoints.api(
     name='heroes_api',
     version='v1',
     description='Heroes API')
 class HeroesApi(remote.Service):
+    """Def of endpoints."""
 
-    # http://localhost:8081/_ah/api/heroes_api/v1/heroes
-    @Hero.query_method(path='heroes', name='heroes.list')
-    def HeroList(self, query):
+    # /_ah/api/heroes_api/v1/heroes
+    @Hero.query_method(path='heroes',
+                       name='heroes.list')
+    def hero_list(self, query):
         return query
 
-    # http://localhost:8081/_ah/api/heroes_api/v1/hero/5222955109842944
+    # /_ah/api/heroes_api/v1/hero/5222955109842944
     @Hero.method(request_fields=('id',),
-                 path='hero/{id}', http_method='GET', name='heroes.get')
-    def HeroGet(self, hero):
+                 path='hero/{id}',
+                 http_method='GET',
+                 name='heroes.get')
+    def hero_get(self, hero):
         if not hero.from_datastore:
             raise endpoints.NotFoundException('Hero not found.')
         return hero
 
-    @Hero.method(path='heroes', http_method='POST', name='heroes.insert')
-    def HeroInsert(self, hero):
+    # /_ah/api/heroes_api/v1/heroes
+    @Hero.method(path='heroes',
+                 http_method='POST',
+                 name='heroes.insert')
+    def hero_insert(self, hero):
         hero.put()
         return hero
 
-    @Hero.method(path='heroes/{id}', http_method='PUT', name='heroes.update')
-    def HeroUpdate(self, hero):
+    # /_ah/api/heroes_api/v1/heroes
+    @Hero.method(path='heroes/{id}',
+                 http_method='PUT',
+                 name='heroes.update')
+    def hero_update(self, hero):
         hero.put()
         return hero
